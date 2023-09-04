e84b3fdab3e45d104091c50496a8415004863fef
Ethan Niser <thebestgamer2502@gmail.com>
2023-09-04 15:51:54 -0500

    config

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,13 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.astro" },
+  { import = "astrocommunity.pack.haskell" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
 }
